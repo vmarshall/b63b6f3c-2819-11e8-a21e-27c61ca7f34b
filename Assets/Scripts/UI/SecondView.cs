dbc549430266d4c010c5bc6d86358d982b0e210b
GregLukosek <lukos86@gmail.com>
2018-01-31 11:11:42 +0000

    Created simple UI with multiple issues

---
+++ Assets/Scripts/UI/SecondView.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UIToolkit.UI;
+
+
+public class SecondView : UIView {
+ 
+	public UIList list;
+
+
+	public override void ViewWillAppear (UIView sourceView = null) {
+		base.ViewWillAppear (sourceView);
+
+
+		//DO NOT MODIFY THE DATA
+		//cell data
+		List<long> data = new List<long>();
+		for (int i=0; i<1000; i++) data.Add(i * 100000000l);
+
+		list.Populate(new ArrayList(data));
+
+
+
+
+
+	}
+
+
+		
+}
